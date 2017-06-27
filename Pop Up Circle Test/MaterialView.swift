349e8a697ed7ee902aa317b11e624093a03b71d2
Joseph Hall <jwhalldesign@gmail.com>
2017-06-27 05:45:39 -0700

    no message

--- Pop Up Circle Test/MaterialView.swift
+++ Pop Up Circle Test/MaterialView.swift
@@ -25,12 +25,12 @@ extension UIView {
             
             if materialKey {
                 
-                self.layer.masksToBounds = false
+              /*  self.layer.masksToBounds = false
                 self.layer.cornerRadius = 3.0
                 self.layer.shadowOpacity = 0.8
                 self.layer.shadowRadius = 3.0
                 self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor */
                 
             } else {
                 
