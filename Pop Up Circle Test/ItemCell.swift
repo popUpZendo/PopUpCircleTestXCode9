5d2a6f37efd64034f4f17494cf14ad4963298db6
Joseph Hall <jwhalldesign@gmail.com>
2017-06-26 19:54:05 -0700

    The schedule page is working but needs formatting.

---
+++ Pop Up Circle Test/ItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemCell.swift
+//  Pop Up Circle Test
+//
+//  Created by Joseph Hall on 6/22/17.
+//  Copyright © 2017 Om Design. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item) {
+        
+        title.text = item.title
+        price.text = "\(item.price)"
+        details.text = item.details
+        thumb.image = item.toImage?.image as? UIImage
+        
+    }
+    
+}
