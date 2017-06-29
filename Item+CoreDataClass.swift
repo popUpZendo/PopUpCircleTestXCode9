41221b704ce62aee62aaf47514ed2199818a8047
Joseph Hall <jwhalldesign@gmail.com>
2017-06-29 16:43:19 -0700

    Just a test message

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by Joseph Hall on 6/29/17.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Item+CoreDataProperties.swift
+//  
+//
+//  Created by Joseph Hall on 6/29/17.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var created: NSDate?
+    @NSManaged public var details: String?
+    @NSManaged public var itemType: String?
+    @NSManaged public var price: Double
+    @NSManaged public var time: String?
+    @NSManaged public var title: String?
+    @NSManaged public var toImage: Image?
+    @NSManaged public var toItemType: ItemType?
+    @NSManaged public var toStore: Store?
+
+}
