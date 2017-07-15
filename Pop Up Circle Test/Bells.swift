d0e1a446addb8aacbd968db2df813518891b4132
Joseph Hall <jwhalldesign@gmail.com>
2017-07-14 19:37:20 -0700

    Item View Controller needs, time and days loaded in coredata

---
+++ Pop Up Circle Test/Bells.swift
@@ -0,0 +1,9 @@
+//
+//  Bells.swift
+//  Pop Up Circle Test
+//
+//  Created by Joseph Hall on 7/14/17.
+//  Copyright © 2017 Om Design. All rights reserved.
+//
+
+import Foundation
--- Pop Up Circle Test/Item+CoreDataProperties.swift
+++ Pop Up Circle Test/Item+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Item+CoreDataProperties.swift
 //  
 //
-//  Created by Joseph Hall on 7/11/17.
+//  Created by Joseph Hall on 7/14/17.
 //
 //
 
@@ -16,15 +16,15 @@ extension Item {
         return NSFetchRequest<Item>(entityName: "Item")
     }
 
+    @NSManaged public var bell: Bool
     @NSManaged public var created: NSDate?
+    @NSManaged public var days: String?
     @NSManaged public var details: String?
+    @NSManaged public var eventTime: NSDate?
     @NSManaged public var itemType: String?
     @NSManaged public var location: String?
     @NSManaged public var time: String?
     @NSManaged public var title: String?
-    @NSManaged public var eventTime: NSDate?
-    @NSManaged public var days: String?
-    @NSManaged public var bell: Bool
     @NSManaged public var toImage: Image?
     @NSManaged public var toItemType: ItemType?
     @NSManaged public var toStore: Store?
