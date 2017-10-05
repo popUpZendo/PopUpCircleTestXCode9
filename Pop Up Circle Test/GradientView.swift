5e9fabe936c0949ead61583bc694f55b3dab80c0
Joseph Hall <jwhalldesign@gmail.com>
2017-10-04 17:41:21 -0700

    Side pullout menu

---
+++ Pop Up Circle Test/GradientView.swift
@@ -0,0 +1,39 @@
+//
+//  GradientView.swift
+//  Pop Up Circle Test
+//
+//  Created by Joseph Hall on 10/4/17.
+//  Copyright © 2017 Om Design. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+        
+    }
+    
+    
+}
+
